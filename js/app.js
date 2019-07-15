4fc72fb6756785399129b4f30f77875e46f4ed67
Isaac Nkoy <nkoylokoteisaacdelor@gmail.com>
2019-07-15 20:09:01 +0200

    some css changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks =document.querySelectorAll('.nav-links li');
+    // 
     burger.addEventListener('clikc', () => {
         nav.classList.toggle('nav-active');
     });
