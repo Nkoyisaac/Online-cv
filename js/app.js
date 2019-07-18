240089ca929c3e9a4ed95f886c71e40cb4f51afc
Isaac Nkoy <nkoylokoteisaacdelor@gmail.com>
2019-07-18 17:50:36 +0200

    few changes in html css and js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks =document.querySelectorAll('.nav-links li');
-    // 
-    burger.addEventListener('clikc', () => {
-        nav.classList.toggle('nav-active');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener("click", () =>{
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle('fade')
     });
-}
-navSlide();
+
+});
