e2df6140f920094be2cdd86615dd48a377216827
Isaac Nkoy <nkoylokoteisaacdelor@gmail.com>
2019-10-25 13:53:58 +0200

    add of projects section

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
-const linksOpen = document.querySelectorAll('.nav-links.open')
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = document.querySelector('.nav-links');
+// const links = document.querySelectorAll('.nav-links li');
+// const linksOpen = document.querySelectorAll('.nav-links.open')
 
-hamburger.addEventListener("click", () =>{
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle('fade')
-    });
+// hamburger.addEventListener("click", () =>{
+//     navLinks.classList.toggle("open");
+//     links.forEach(link => {
+//         link.classList.toggle('fade')
+//     });
 
-});
+// });
